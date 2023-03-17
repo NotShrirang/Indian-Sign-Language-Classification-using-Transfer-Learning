a93994c61223a4c63f4dcb2ab8273f3adfbfa489
Shrirang Mahajan <85283622+NotShrirang@users.noreply.github.com>
2023-03-17 07:51:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,6 +46,7 @@ def preprocessing(img):
     img = img.astype(np.float64)
     img = np.array(img.resize((224, 224))).astype(np.float32)
     img = np.expand_dims(img, axis=0)
+    img = np.expand_dims(img, axis=-1)
     img = img/255
     return img
 
